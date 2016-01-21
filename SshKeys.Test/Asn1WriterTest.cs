4ad686ec3ea73c7009f73cfc1a92221ca62b346a
Martin Dätz <martin@daetz.de>
2016-01-21 14:03:38 +0100

    asn1 test passed

--- SshKeys.Test/Asn1WriterTest.cs
+++ SshKeys.Test/Asn1WriterTest.cs
@@ -17,10 +17,18 @@ namespace SshKeys.Test
 
             var writer = new Asn1Writer(0x240);
 
+            data[0] &= 0x7f;
             writer.WriteInt(data);
 
-            data = writer.GetSequence();
+            var actual = writer.GetSequence();
 
+            Expect(actual[7], Is.EqualTo(0));
+
+            data[0] |= 0x80;
+            writer.WriteInt(data);
+
+            actual = writer.GetSequence();
+            Expect(actual[7], Is.EqualTo(1));
         }
     }
 }
