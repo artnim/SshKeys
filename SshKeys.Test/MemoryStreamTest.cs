ef7648f91665eb1043a8022ab166a75f3a24ef1c
Martin Daetz <martin@daetz.de>
2016-01-21 09:47:40 +0100

    add Tests

---
+++ SshKeys.Test/MemoryStreamTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace SshKeys.Test
+{
+    [TestFixture]
+    public class MemoryStreamTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Expect()
+        }
+
+    }
+}
---
+++ SshKeys/KeyPairRSA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SshKeys
+{
+    class KeyPairRSA
+    {
+        private RSAParameters _keyparams;
+
+        KeyPairRSA(int keysize = 1024)
+        {
+            var rsa = new RSACryptoServiceProvider(keysize);
+            _keyparams = rsa.ExportParameters(true);
+        }
+    }
+}
